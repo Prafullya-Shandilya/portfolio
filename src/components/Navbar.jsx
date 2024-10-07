026f6b31420cf8dfb5ff2e725be70dd18647f712
Prafullya Shandilya <110183335+Prafullya-Shandilya@users.noreply.github.com>
2024-10-07 19:31:02 +0530

    social media link updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,11 +10,19 @@ const Navbar = () => {
       <div className="flex flex-shrink-0 items-cente">
         <img className="mx-2 w-20" src={logo} alt="logo" />
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl  ">
+        <a href="https://www.linkedin.com/in/prafullya-shandilya-10b81a236/?trk=opento_sprofile_details">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Prafullya-Shandilya">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/shandilya1445/">
+          <FaInstagram />
+        </a>
+        <a href="#">
+          <FaSquareXTwitter />
+        </a>
       </div>
     </nav>
   );
