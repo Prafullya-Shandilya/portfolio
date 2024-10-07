6f3b9e130ca987b84d1c2c76fcade78707ea53be
Prafullya Shandilya <110183335+Prafullya-Shandilya@users.noreply.github.com>
2024-10-07 18:36:43 +0530

    first time add kr rha hu

---
+++ src/constants/index.js
@@ -0,0 +1,80 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a B.Tech graduate in Computer Science and Engineering, with a passion for technology and innovation. I have a solid foundation in HTML, CSS, and Python, and I am currently enhancing my skills in cybersecurity, having completed basic training from Cisco and actively practicing on TryHackMe.In addition to my technical skills in HTML, CSS, and Python, I have completed basic cybersecurity training from Cisco and actively practice on TryHackMe to enhance my knowledge in the field.`;
+
+export const ABOUT_TEXT = `Hi, I’m Prafullya Shandilya, a dedicated web developer and UI/UX enthusiast with expertise in the MERN stack. I thrive on building responsive and engaging websites, blending functionality with elegant design to deliver user-friendly digital solutions. My passion for crafting seamless experiences is reflected in every project I undertake, ensuring both aesthetics and performance are prioritized.
+
+With a B.Tech in Computer Science and Engineering and hands-on experience in frontend development, I bring a well-rounded approach to problem-solving. Additionally, my training in cybersecurity from Cisco and practical work on platforms like TryHackMe enable me to develop solutions with security in mind.
+
+Beyond technology, I am a state-level basketball player, with a love for sports and teamwork that translates into my professional collaborations. My portfolio is a testament to my technical expertise, creativity, and ability to deliver results, both on and off the court.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Frontend Developer",
+    company: "Personal/College Projects.",
+    description: `Worked on various web development projects during my time in college, gaining hands-on experience with React, Node.js, and MongoDB. Designed and implemented RESTful APIs and connected web applications with MongoDB databases. Collaborated with peers and faculty to define project requirements and timelines, simulating a real-world development environment.`,
+    technologies: [ "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Personal/College Projects",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Web Development",
+    company: "Personal/College Projects",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "JavaScript", "HTML", "CSS"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Personal ",
+    description: ` I immersed myself in college life, actively participating in various projects and honing my skills. This period was pivotal as I explored different career paths, ultimately guiding me toward a focus on web development and full-stack technologies, while considering where to apply my talents in the professional world.`,
+    technologies: ["HTML", "CSS", "mySQL", "C", "C++"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Meal Master",
+    image: project1,
+    description:
+      "Meal Master is a dynamic web application designed to display a wide variety of food options based on user preferences. Users can browse or search for meals, and the site will provide curated results tailored to their choices. This project allowed me to focus on building responsive interfaces and managing data efficiently to ensure a smooth user experience. It showcases my ability to handle complex data requests and deliver relevant results in real-time.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Clone Of Netflix",
+    image: project2,
+    description:
+      "We developed a Netflix clone as part of my college projects to enhance my skills in full-stack development. The project replicates core features of the popular streaming platform, including a user-friendly interface, video listings, and search functionality. The app manages user accounts, favorite lists, and watch histories, demonstrating my ability to build scalable web applications and manage dynamic content efficiently.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Hostel Vibhag",
+    image: project4,
+    description:
+      "Hostel Vibhag is a comprehensive hostel management system designed to simplify room booking for students. The platform enables online room applications, seat allocations, and real-time availability searches. It also offers additional features like raising complaints and managing gate passes, ensuring a seamless experience for both students and administrators.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Begusarai, Bihar ",
+  phoneNo: "+916206519194 ",
+  email: "prafulshandilya1111@gmail.com",
+};
